579b9dbe62b682c03e0ccf765c8f3ebbbb9b0446
WZTEO <WZTEO69@GMAIL.COM>
2025-10-10 16:21:05 -0700

    Update admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -1,8 +1,3 @@
-from django.contrib import admin
-from .models import WithdrawalRequest
+from django.db import models
 
-@admin.register(WithdrawalRequest)
-class WithdrawalRequestAdmin(admin.ModelAdmin):
-    list_display = ('user', 'recipient', 'amount', 'status', 'created_at', 'processed_at')
-    list_filter = ('status', 'created_at')
-    search_fields = ('user__username', 'recipient__recipient_name')
+# create your models here.
